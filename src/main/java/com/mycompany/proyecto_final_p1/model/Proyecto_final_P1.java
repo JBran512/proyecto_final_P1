62a2b9bd5c4a412df44444a34d24450a401cc46e
Jafet <branjafet1@gmail.com>
2026-04-27 21:46:36 -0600

    subida de modulos iniciales

---
+++ src/main/java/com/mycompany/proyecto_final_p1/model/Proyecto_final_P1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_final_p1.model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Proyecto_final_P1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
