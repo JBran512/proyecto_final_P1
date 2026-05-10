8c23cb2ee25e9d3240ba049bb4e180a0ab7c364e
Chrisitan Blanco <cfbmorales@gmail.com>
2026-05-10 00:22:34 -0600

    creación de métodos en el controlador

---
+++ src/main/java/com/mycompany/proyecto_final_p1/logic/PagoController.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_final_p1.logic;
+
+/**
+ *
+ * @author Admin
+ */
+public class PagoController {
+    private String metodoPago;
+    private String estadoPago;
+    private String fechaPago;
+    private int idPago;
+    private int idCliente;
+    private double montoPagado;
+
+    public PagoController(String metodoPago, String estadoPago, String fechaPago, int idPago, int idCliente, double montoPagado) {
+        this.metodoPago = metodoPago;
+        this.estadoPago = estadoPago;
+        this.fechaPago = fechaPago;
+        this.idPago = idPago;
+        this.idCliente = idCliente;
+        this.montoPagado = montoPagado;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(String metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public String getEstadoPago() {
+        return estadoPago;
+    }
+
+    public void setEstadoPago(String estadoPago) {
+        this.estadoPago = estadoPago;
+    }
+
+    public String getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(String fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public int getIdPago() {
+        return idPago;
+    }
+
+    public void setIdPago(int idPago) {
+        this.idPago = idPago;
+    }
+
+    public double getMontoPagado() {
+        return montoPagado;
+    }
+
+    public void setMontoPagado(double montoPagado) {
+        this.montoPagado = montoPagado;
+    }
+    
+    
+}
