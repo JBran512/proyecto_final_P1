71378dc647cfd741f3a5224a092df148c6109bcd
Jafet <branjafet1@gmail.com>
2026-05-12 12:28:04 -0600

    Se agrega Conexion, UsuarioDAO, modelos Pago y Propietario corregidos, conflictos anteriores resueltos

--- src/main/java/com/mycompany/proyecto_final_p1/model/Propietario.java
+++ src/main/java/com/mycompany/proyecto_final_p1/model/Propietario.java
@@ -2,19 +2,51 @@ package com.mycompany.proyecto_final_p1.model;
 
 public class Propietario {
     //09/05/2026 By Angel Cinto - "Agregan campos necesarios para la clase Propietario"
+    private int idPropietario;
     private String nombre;
     private String telefono;
     private String correo;
-    private int numeroCasa;
     //09/05/2026 By Angel Cinto
     // CONSTRUCTOR 1: El que recibe todo (Generado con Alt+Insert)
-    public Propietario(String nombre, String telefono, String correo, int numeroCasa) {
+    public Propietario(String nombre, String telefono, String correo) {
         this.nombre = nombre;
         this.telefono = telefono;
         this.correo = correo;
-        this.numeroCasa = numeroCasa;
     }
     // CONSTRUCTOR 2: El vacío (Para procesos automáticos)
     public Propietario() {
+    }
+
+    // Getters y Setters
+    public int getIdPropietario() {
+        return idPropietario;
+    }
+
+    public void setIdPropietario(int idPropietario) {
+        this.idPropietario = idPropietario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 }
