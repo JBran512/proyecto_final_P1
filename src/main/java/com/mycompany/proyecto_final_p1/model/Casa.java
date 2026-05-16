fcdfdb19c85525605ad4ca0298fc3fa6f9154b24
Jafet <branjafet1@gmail.com>
2026-05-15 21:57:05 -0600

    feat: agregar CasaDAO, Casa model, LoginView, CasaView, IngresarCasaDialog y ModificarCasaDialog

--- src/main/java/com/mycompany/proyecto_final_p1/model/Casa.java
+++ src/main/java/com/mycompany/proyecto_final_p1/model/Casa.java
@@ -9,5 +9,52 @@ package com.mycompany.proyecto_final_p1.model;
  * @author Admin
  */
 public class Casa {
-    
+    private int idCasa;
+    private int numeroCasa;
+    private int idPropietario;
+    private int idCondominio;
+
+    // Constructor para insertar
+    public Casa(int numeroCasa, int idPropietario, int idCondominio) {
+        this.numeroCasa = numeroCasa;
+        this.idPropietario = idPropietario;
+        this.idCondominio = idCondominio;
+    }
+
+    // Constructor vacío para procesos automáticos
+    public Casa() {
+    }
+
+    // Getters y Setters
+    public int getIdCasa() {
+        return idCasa;
+    }
+
+    public void setIdCasa(int idCasa) {
+        this.idCasa = idCasa;
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public int getIdPropietario() {
+        return idPropietario;
+    }
+
+    public void setIdPropietario(int idPropietario) {
+        this.idPropietario = idPropietario;
+    }
+
+    public int getIdCondominio() {
+        return idCondominio;
+    }
+
+    public void setIdCondominio(int idCondominio) {
+        this.idCondominio = idCondominio;
+    }
 }
