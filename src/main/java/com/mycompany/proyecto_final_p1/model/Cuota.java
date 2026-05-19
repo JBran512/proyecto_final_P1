f288cf1bc04fdaa6769e55bf993f4ec10fd9cd16
Jafet <branjafet1@gmail.com>
2026-05-19 12:40:55 -0600

    Se agrega Cuota model y CuotaDAO

---
+++ src/main/java/com/mycompany/proyecto_final_p1/model/Cuota.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_final_p1.model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Cuota {
+    private int idCuota;
+    private int mes;
+    private int anio;
+    private int monto;
+    private int createdBy;
+    
+    public Cuota(int mes, int anio, int monto){
+        this.mes = mes;
+        this.anio = anio;
+        this.monto = monto;
+    }
+    
+    public Cuota() {
+    }
+
+    public int getIdCuota() {
+        return idCuota;
+    }
+
+    public void setIdCuota(int idCuota) {
+        this.idCuota = idCuota;
+    }
+    
+    public int getMes(){
+        return mes;
+    }
+    
+    public void setMes(int mes){
+        this.mes = mes;
+    }
+    
+    public int getAnio(){
+        return anio;
+    }
+    
+    public void setAnio(int anio){
+        this.anio = anio;
+    }
+    
+    public int getMonto(){
+        return monto;
+    }
+    
+    public void setMonto(int monto){
+        this.monto = monto;
+    }
+    
+    public int getCreatedBy(){
+        return createdBy;
+    }
+    
+    public void setCreatedBy(int createdBy){
+        this.createdBy = createdBy;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/proyecto_final_p1/util/CuotaDAO.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_final_p1.util;
+
+import com.mycompany.proyecto_final_p1.model.Cuota;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class CuotaDAO {
+    public List<Cuota> listarCuotas() throws SQLException{
+        List<Cuota> lista = new ArrayList<>();
+        try{
+            Connection con = Conexion.getConexion();
+            PreparedStatement ps = con.prepareStatement(
+                "SELECT * FROM cuota"
+            );
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                Cuota c = new Cuota();
+                c.setIdCuota(rs.getInt("id_cuota"));
+                c.setMes(rs.getInt("mes"));
+                c.setAnio(rs.getInt("anio"));
+                c.setMonto(rs.getInt("monto"));
+                lista.add(c);
+            }
+        } catch (SQLException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        return lista;
+    }
+    
+    public boolean insertar(Cuota cuota) throws SQLException{
+       try{
+           Connection con = Conexion.getConexion();
+           PreparedStatement ps = con.prepareStatement(
+                "INSERT INTO cuota (mes, anio, monto, created_by) VALUES (?, ?, ?, ?)"
+            );
+           ps.setInt(1, cuota.getMes());
+           ps.setInt(2, cuota.getAnio());
+           ps.setInt(3, cuota.getMonto());
+           ps.setInt(4, cuota.getCreatedBy());
+           return ps.executeUpdate() > 0;
+       } catch (SQLException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+       return false;
+    }
+    
+    public Cuota obtenerCuotaPorMes(int mes, int anio) throws SQLException{
+        try{
+            Connection con = Conexion.getConexion();
+            PreparedStatement ps = con.prepareStatement(
+                "SELECT * FROM cuota WHERE mes = ? AND anio = ?"
+            );
+            ps.setInt(1, mes);
+            ps.setInt(2, anio);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                Cuota c = new Cuota();
+                c.setIdCuota(rs.getInt("id_cuota"));
+                c.setMes(rs.getInt("mes"));
+                c.setAnio(rs.getInt("anio"));
+                c.setMonto(rs.getInt("monto"));
+                return c;
+            }
+        } catch (SQLException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        return null;
+    }
+    
+    public boolean existeCuota(int mes, int anio) throws SQLException{
+        try{
+            Connection con = Conexion.getConexion();
+            PreparedStatement ps = con.prepareStatement(
+                "SELECT * FROM cuota WHERE mes = ? AND anio = ?"
+            );
+            ps.setInt(1, mes);
+            ps.setInt(2, anio);
+            ResultSet rs = ps.executeQuery();
+            return rs.next();
+        }catch (SQLException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        return false;
+    }
+}
