406704d461b6df49bea1a2fb109ac6ab68eb352c
Jafet <branjafet1@gmail.com>
2026-05-20 21:50:49 -0600

    Se integra cobros con generarPagos, actualizar clase Cobros con id_cuota y se agregarn validaciones en CobrosView, obtenerUltimoCobro en CobrosDAO

--- src/main/java/com/mycompany/proyecto_final_p1/util/CobrosDAO.java
+++ src/main/java/com/mycompany/proyecto_final_p1/util/CobrosDAO.java
@@ -9,34 +9,64 @@ import java.sql.SQLException;
 import com.mycompany.proyecto_final_p1.util.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import javax.swing.JOptionPane;
+import java.sql.Statement;
+import java.util.Calendar;
 
 /**
  *
  * @author Chriss
  */
 public class CobrosDAO {
-    
-    public int realizarCobro(Cobros cobros){
-        
-    try{
-          Connection con = Conexion.getConexion();
-          String sql = "INSERT INTO cobros(monto, fechaInicio, fechaLimite, createdBy) values (?,?,?,?)";
-          
-          PreparedStatement ps = con.prepareStatement(sql);
-          ps.setInt(1, cobros.getMonto());
-          ps.setDate(2, cobros.getFechaInicio());
-          ps.setDate(3, cobros.getFechaLimite());
-          ps.setInt(4, cobros.getCreatedBy());
-          
-          ps.executeUpdate();
-          JOptionPane.showMessageDialog(null, "Datos guardados", "Cobros", JOptionPane.INFORMATION_MESSAGE);
-          return 1;
-          
-    }catch(SQLException e){
-        System.out.println(e.getMessage());
-        JOptionPane.showMessageDialog(null, "No se guardaron los datos", "Cobros", JOptionPane.INFORMATION_MESSAGE);
+
+    public int realizarCobro(Cobros cobros) {
+
+        try {
+            Connection con = Conexion.getConexion();
+            String sql = "INSERT INTO cobros(id_cuota, fechaInicio, fechaLimite, createdBy) values (?,?,?,?)";
+
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, cobros.getIdCuota());
+            ps.setDate(2, cobros.getFechaInicio());
+            ps.setDate(3, cobros.getFechaLimite());
+            ps.setInt(4, cobros.getCreatedBy());
+
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                JOptionPane.showMessageDialog(null, "Datos guardados", "Cobros", JOptionPane.INFORMATION_MESSAGE);
+                return rs.getInt(1); // retorna el id_cobro real
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "No se guardaron los datos", "Cobros", JOptionPane.INFORMATION_MESSAGE);
+        }
+        return -1;
     }
-         return -1;
-  }
-}  
+    
+    public Cobros obtenerUlimoCobro() throws SQLException{
+        try{
+            Connection con = Conexion.getConexion();
+            PreparedStatement ps = con.prepareStatement(
+                "SELECT * FROM cobros ORDER BY id_cobro DESC LIMIT 1"
+            );
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                Cobros c = new Cobros();
+                c.setIdCobro(rs.getInt("id_cobro"));
+                java.util.Date fecha = rs.getDate("fechaInicio");
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(fecha);
+                c.setMes(cal.get(Calendar.MONTH)+1);
+                c.setAnio(cal.get(Calendar.YEAR));
+                return c;
+            } 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "No se completo el proceso", "Cobros", JOptionPane.INFORMATION_MESSAGE);
+        }
+        return null;
+    }
+}
